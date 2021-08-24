be69017952525bda2e70615d5698d0e09db64a8f
山崎満 <zakiyama.com12.aaa@gmail.com>
2021-08-24 20:48:52 +0900

    ルーティング記載

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/reply/like/{id}', 'FacilityController@like')->name('facility.like');
+Route::get('/reply/unlike/{id}', 'FacilityController@unlike')->name('facility.unlike');
