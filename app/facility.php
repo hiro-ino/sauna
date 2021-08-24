3097f54baa3482389a3f8515741243080c344dfb
km9990 <keito0760@gmail.com>
2021-08-24 23:46:59 +0900

    commit2

--- app/facility.php
+++ app/facility.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Facility extends Model
 {
-    function user()
-    {
-        return $this->belongsTo('App\User');
-    }
     function facilityCategories()
     {
         return $this->hasMany('App\FacilityCategory');
